44a9e5b52e4ef88e7d822d268a2a26bc0836b466
Lemashon <lemakipeno@gmail.com>
2023-01-05 23:02:02 +0300

    second commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,12 @@
-import express from "express";
-import { login, register } from "../controllers/auth.js";
-
+import express from "express"
 const router = express.Router();
 
-router.post("/register", register)
-router.post("/login", login)
+router.get("/", (req,res)=>{
+    res.send("Hello, this is auth endpoint")
+})
+router.get("/register", (req,res)=>{
+    res.send("Hello, this is auth register endpoint")
+
+})
 
 export default router
