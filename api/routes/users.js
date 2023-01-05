4ae671479cf411c19128a40108e1f35c05c735ca
Lemashon <lemakipeno@gmail.com>
2023-01-06 02:35:37 +0300

    second commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,17 @@
 import express from "express"
 import { updateUser,deleteUser,getUser,getUsers } from "../controllers/user.js";
+import {  verifyToken,verifyUser,verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
+router.get("/checkauthentication", verifyToken,(req,res,next)=>{
+    res.send("hello user, you are logged in!");
+})
+router.get("/checkuser/:id", verifyUser ,(req,res,next)=>{
+    res.send("hello user, you are logged in and you can delete your account!");
+})
+router.get("/checkadmin/:id", verifyUser ,(req,res,next)=>{
+    res.send("hello admin, you are logged in and you can delete all accounts!");
+})
 
 
 
